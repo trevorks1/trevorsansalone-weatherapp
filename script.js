67dc17824169a61da2424cd7f1a9b7c774a3912b
TrevorKS1 <trevorksansalone@gmail.com>
2021-04-15 08:02:04 -0500

    creating the fetchWeather function for use of city and metric system with Error response and data pull

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let weather = {
   apiKey: '323bdc6b0c610f2a39bc7968794d1e31',
+  fetchWeather: function (city) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=metric&appid=' +
+        this.apikey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert('No weather found.');
+          throw new Error('No weather found.');
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
 };
